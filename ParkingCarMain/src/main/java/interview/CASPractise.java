e33b8a9229cd2a572ca218a41125f5e0e8110fa0
shikeyue <xinluqishi@126.com>
2017-06-13 09:38:42 +0800

    Related to CAS practices

--- ParkingCarMain/src/main/java/interview/CASPractise.java
+++ ParkingCarMain/src/main/java/interview/CASPractise.java
@@ -1,6 +1,9 @@
 package interview;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicStampedReference;
 
 /**
  * Created by shikeyue on 17/6/13.
@@ -8,5 +11,86 @@ import java.util.concurrent.atomic.AtomicReference;
 public class CASPractise {
 
     private AtomicReference atomicReference = new AtomicReference();
+
+    private static AtomicInteger atomicInt = new AtomicInteger(100);
+
+    private static AtomicStampedReference<Integer> atomicStampedRef = new AtomicStampedReference<>(100, 0);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread intT1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                atomicInt.compareAndSet(100, 101);
+                atomicInt.compareAndSet(101, 100);
+            }
+        });
+
+        Thread intT2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                boolean c3 = atomicInt.compareAndSet(100, 101);
+                System.out.println(c3);
+            }
+        });
+
+        intT1.start();
+        intT2.start();
+        intT1.join();
+        intT2.join();
+
+
+        Thread refT1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                atomicStampedRef.compareAndSet(100, 101,
+                        atomicStampedRef.getStamp(), atomicStampedRef.getStamp() + 1);
+
+                atomicStampedRef.compareAndSet(101, 100,
+                        atomicStampedRef.getStamp(), atomicStampedRef.getStamp() + 1);
+
+            }
+        });
+
+        Thread refT2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int stamp = atomicStampedRef.getStamp();
+                System.out.println("Before sleep : stamp = " + stamp);
+
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("After sleep : stamp = " + atomicStampedRef.getStamp());
+                boolean c3 = atomicStampedRef.compareAndSet(100, 101, stamp, stamp + 1);
+
+                System.out.println(c3);
+
+            }
+        });
+
+        refT1.start();
+        refT2.start();
+
+        refT1.join();
+        refT2.join();
+
+    }
+
+
 
 }
