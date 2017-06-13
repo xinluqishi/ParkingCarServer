88751cee04d52c443bc8684e85f92a344c1b2caf
shikeyue <xinluqishi@126.com>
2017-06-13 19:53:22 +0800

    Some knowledge for thread and algorithm

---
+++ ParkingCarMain/src/main/java/interview/Algorithm.java
@@ -0,0 +1,12 @@
+package interview;
+
+/**
+ * Created by shikeyue on 2017/6/13.
+ */
+public class Algorithm {
+
+    public class Node {
+
+    }
+
+}
--- ParkingCarMain/src/main/java/interview/PoolThread.java
+++ ParkingCarMain/src/main/java/interview/PoolThread.java
@@ -1,7 +1,17 @@
 package interview;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by shikeyue on 2017/6/12.
  */
 public class PoolThread {
+
+    private ReentrantLock reentrantLock = new ReentrantLock();
+
+    private void tryReentryLockMethod() throws InterruptedException {
+        reentrantLock.tryLock();
+        reentrantLock.lockInterruptibly();
+    }
+
 }
