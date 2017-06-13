faed7b6e7667a6b475425bda61fbf3b6ad367f29
shikeyue <xinluqishi@126.com>
2017-06-13 08:51:42 +0800

    Related to CAS practices

---
+++ ParkingCarMain/src/main/java/interview/CASPractise.java
@@ -0,0 +1,12 @@
+package interview;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * Created by shikeyue on 17/6/13.
+ */
+public class CASPractise {
+
+    private AtomicReference atomicReference = new AtomicReference();
+
+}
