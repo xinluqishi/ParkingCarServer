128cdb4ca25b4bcd71ed09b2c1e574be69c88f17
shikeyue <xinluqishi@126.com>
2017-06-23 16:29:08 +0800

    Add Executor function

---
+++ ParkingCarMain/src/main/java/interview/ConcurrentLinkedQ.java
@@ -0,0 +1,29 @@
+package interview;
+
+import sun.misc.Unsafe;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * Created by shikeyue on 17/6/18.
+ */
+public class ConcurrentLinkedQ {
+
+    private static class Node<E> {
+        volatile E item;
+        volatile Node<E> next;
+
+        boolean casItem(E cmp, E val) {
+//            return UNSAFE.compareAndSwapObject(this, itemoffset, cmp, val);
+            return false;
+        }
+
+
+
+    }
+
+
+    private ConcurrentLinkedQueue<String> clq = new ConcurrentLinkedQueue<>();
+
+
+}
